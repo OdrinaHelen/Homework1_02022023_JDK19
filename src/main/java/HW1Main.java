3651a3a73a615828bc1929d42ade776c04c06ed7
OdrinaHelen <odrinahelen@gmail.com>
2023-02-05 05:46:34 +0100

    Commit

---
+++ src/main/java/HW1Main.java
@@ -0,0 +1,22 @@
+public class HW1Main {
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = -32000;
+        int c = 200000;
+        long d = -900000L;
+        float e = 67.6543f;
+        double f = 876.6542;
+        char g = 'r';
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
